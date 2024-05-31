9f7856027e3450d29bc598de8bfe13d1bddb8b21
Marshal-Emanuel <marshcodes@gmail.com>
2024-06-01 00:52:45 +0300

    final push

--- js/purchase-history.js
+++ js/purchase-history.js
@@ -1,13 +1,13 @@
 "use strict";
-document.addEventListener('DOMContentLoaded', () => {
-    const historyItemsContainer = document.querySelector('.historyItems');
-    let purchaseHistory = JSON.parse(localStorage.getItem('purchaseHistory') || '[]');
+document.addEventListener("DOMContentLoaded", () => {
+    const historyItemsContainer = document.querySelector(".historyItems");
+    let purchaseHistory = JSON.parse(localStorage.getItem("purchaseHistory") || "[]");
     if (!historyItemsContainer) {
-        console.error('History items container element not found');
+        console.error("History items container element not found");
         return;
     }
     if (purchaseHistory.length === 0) {
-        historyItemsContainer.innerText = 'You have no purchase history';
+        historyItemsContainer.innerText = "You have no purchase history";
         return;
     }
     purchaseHistory.forEach((product) => {
@@ -17,17 +17,17 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
     function createHistoryProduct(productData) {
-        let historyProduct = document.createElement('div');
-        historyProduct.classList.add('historyProduct');
-        let productImage = document.createElement('img');
-        productImage.className = 'productImagee';
+        let historyProduct = document.createElement("div");
+        historyProduct.classList.add("historyProduct");
+        let productImage = document.createElement("img");
+        productImage.className = "productImagee";
         productImage.src = productData.imageURL;
-        let productName = document.createElement('div');
-        productName.classList.add('productName');
+        let productName = document.createElement("div");
+        productName.classList.add("productName");
         productName.innerText = productData.name;
-        let productPrice = document.createElement('div');
-        productPrice.classList.add('productPrice');
-        productPrice.innerText = 'Price: Ksh ' + productData.price;
+        let productPrice = document.createElement("div");
+        productPrice.classList.add("productPrice");
+        productPrice.innerText = "Price: Ksh " + productData.price;
         historyProduct.appendChild(productImage);
         historyProduct.appendChild(productName);
         historyProduct.appendChild(productPrice);
